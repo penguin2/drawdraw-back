3f1a7b70bbe77ee5208d8d663535e05c46d523f9
penguin2 <17985971+penguin2@users.noreply.github.com>
2020-12-08 20:24:07 +0900

    test

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {    
       res.json({ "paletteColors": ["black", "white", "orange"] });
     });
+    
 app.get("/color", (req, res) => {    
     res.json({ "paletteColors": ["black", "white", "orange"] });
     });
