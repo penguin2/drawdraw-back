45835b2dd33b03f67ebba129cb2891693d2db4d2
penguin2 <17985971+penguin2@users.noreply.github.com>
2020-12-14 23:10:53 +0900

    formidable save image

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
- 
+const formidable = require('formidable');
+const fs = require('fs');
 const app = express();
 require('dotenv').config();
 
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
- require('dotenv').config();
+require('dotenv').config();
 
 app.get("/", (req, res) => {    
       res.json({ "paletteColors": ["black", "white", "orange"] });
@@ -18,7 +19,23 @@ app.get("/", (req, res) => {
 app.get("/color", (req, res) => {    
     res.json({ "paletteColors": ["black", "white", "orange"] });
     });
-  
+
+app.post("/upload-image", async (req, res) => {
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files, image) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    const base64Data = fields.image.replace("data:image\/png;base64,", "");
+    fs.writeFile(fields.description+'.png', base64Data, "base64", (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+    res.json({ "hello":"world" });
+  });  
+});
     
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
